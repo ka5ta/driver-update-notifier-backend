f63119c7843186be5370b11e21ac40d087680c73
ka5ta <55314237+ka5ta@users.noreply.github.com>
2021-12-28 16:25:57 +0100

    create Service for automatic task to update Drivers listed in database. Enabled task scheduling.

---
+++ src/main/java/com/ka5ta/drivers/Services/UpdateDriversService.java
@@ -0,0 +1,48 @@
+package com.ka5ta.drivers.Services;
+
+import com.ka5ta.drivers.DriversApplication;
+
+import com.ka5ta.drivers.Entities.Product;
+import com.ka5ta.drivers.Repositories.DriverRepository;
+import com.ka5ta.drivers.Repositories.ProductRepository;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UpdateDriversService {
+
+    private static final Logger log = LoggerFactory.getLogger(UpdateDriversService.class);
+
+    @Autowired
+    private DriverService driverService;
+    @Autowired
+    private ProductRepository productRepository;
+
+//private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    @Scheduled(cron = "0 */5 * ? * *")
+    private void runUpdateDrivers() {
+        updateDrivers();
+        log.info("Drivers are now updated");
+    }
+
+    private void updateDrivers() {
+        List<String> downloadLinks = productRepository.getDownloadLinks();
+        downloadLinks.forEach(link -> {
+            try {
+                Product productFromLink = driverService.getProductFromLink(link);
+                log.info(productFromLink.getName() + " was updated");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+}

fa7205a6391a7d7df19b8688dcae43e2ae5b9de7
ka5ta <55314237+ka5ta@users.noreply.github.com>
2022-01-26 13:52:46 +0100

    changed server ip to allow lan access

--- src/main/java/com/ka5ta/drivers/Controllers/EmailController.java
+++ src/main/java/com/ka5ta/drivers/Controllers/EmailController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://192.168.1.54:4200/", maxAge = 3600)
 @RestController
 @RequestMapping("api")
 public class EmailController {

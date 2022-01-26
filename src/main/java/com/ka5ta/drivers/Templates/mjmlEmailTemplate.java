abcf23ee862b8f25627062758df39d30b1318de6
ka5ta <55314237+ka5ta@users.noreply.github.com>
2022-01-26 15:17:27 +0100

    merge conflict fixes

--- src/main/java/com/ka5ta/drivers/Templates/mjmlEmailTemplate.java
+++ src/main/java/com/ka5ta/drivers/Templates/mjmlEmailTemplate.java
@@ -1,18 +1,16 @@
 package com.ka5ta.drivers.Templates;
 
-
-import es.atrujillo.mjml.config.template.TemplateFactory;
-import io.camassia.mjml.MJMLClient;
+//import io.camassia.mjml.MJMLClient;
 import org.springframework.stereotype.Controller;
-import org.thymeleaf.context.Context;
+
 
 import java.io.File;
 
 @Controller
 public class mjmlEmailTemplate {
 
-    MJMLClient client = MJMLClient.newDefaultClient()
-            .withApplicationID("...")
-            .withApplicationKey("...");
+//    MJMLClient client = MJMLClient.newDefaultClient()
+//            .withApplicationID("...")
+//            .withApplicationKey("...");
 
 }

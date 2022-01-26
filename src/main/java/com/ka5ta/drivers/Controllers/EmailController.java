f60f35c3c705efeb3dae8dce7566a016bb197443
ka5ta <55314237+ka5ta@users.noreply.github.com>
2022-01-26 14:21:26 +0100

    remove unused imports

--- src/main/java/com/ka5ta/drivers/Controllers/EmailController.java
+++ src/main/java/com/ka5ta/drivers/Controllers/EmailController.java
@@ -7,23 +7,12 @@ import com.ka5ta.drivers.Entities.Product;
 import com.ka5ta.drivers.Repositories.SubscriptionRepository;
 import com.ka5ta.drivers.Services.EmailService;
 import com.ka5ta.drivers.Services.SubscriptionService;
-import j2html.tags.ContainerTag;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.ArrayList;
-import java.util.List;
 
-import static j2html.TagCreator.*;
 
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
